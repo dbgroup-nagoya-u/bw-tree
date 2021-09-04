390f91c17ba38ce648623eaff5a1b73254ed2a07
Kento Sugiura <sugiura@db.is.i.nagoya-u.ac.jp>
2021-09-04 11:40:56 +0900

    add dummy tests to supress include errors

---
+++ test/node_test.cpp
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2021 Database Group, Nagoya University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "bw_tree/component/node.hpp"
+
+#include "gtest/gtest.h"
+
+namespace dbgroup::index::bw_tree::component::test
+{
+class NodeFixture : public testing::Test
+{
+ protected:
+  /*################################################################################################
+   * Internal constants
+   *##############################################################################################*/
+
+  /*################################################################################################
+   * Setup/Teardown
+   *##############################################################################################*/
+
+  void
+  SetUp() override
+  {
+  }
+
+  void
+  TearDown() override
+  {
+  }
+};
+
+/*--------------------------------------------------------------------------------------------------
+ * Constructor tests
+ *------------------------------------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------------------------------------------
+ * Getter/setter tests
+ *------------------------------------------------------------------------------------------------*/
+
+}  // namespace dbgroup::index::bw_tree::component::test
