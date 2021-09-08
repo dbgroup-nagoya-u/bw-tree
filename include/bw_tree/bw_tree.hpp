dee18754bb83f54e6b860086b35285b8f8271c61
Kento Sugiura <sugiura@db.is.i.nagoya-u.ac.jp>
2021-09-08 14:34:12 +0900

    add leaf traverse function

--- include/bw_tree/bw_tree.hpp
+++ include/bw_tree/bw_tree.hpp
@@ -68,6 +68,91 @@ class BwTree
    * Internal utility functions
    *##############################################################################################*/
 
+  void
+  SearchLeafNode(  //
+      const Key &key,
+      const bool range_closed,
+      Mapping_t *page_id,
+      NodeStack_t &stack,
+      Mapping_t *&consolidate_node)
+  {
+    Node_t *current_head = page_id->load(mo_relax);
+    stack.emplace_back(page_id);
+
+    // traverse a Bw-tree
+    while (!current_head->IsLeaf()) {
+      size_t delta_chain_length = 0;
+
+      // traverse a delta chain
+      while (true) {
+        switch (current_head->GetDeltaNodeType()) {
+          case DeltaNodeType::kInsert:
+          case DeltaNodeType::kDelete:
+            // check whether this delta record includes a target key
+            const auto meta0 = current_head->GetMetadata(0), meta1 = current_head->GetMetadata(1);
+            const auto key0 = current_head->GetKey(meta0), key1 = current_head->GetKey(meta1);
+            if (component::IsInRange(key, key0, !range_closed, key1, range_closed)) {
+              page_id = current_head->template GetPayload<Mapping_t *>(meta0);
+              ++delta_chain_length;
+              goto found_child_node;
+            }
+            break;
+
+          case DeltaNodeType::kRemoveNode:
+            // this node is deleted, retry until a delete-index-entry delta is inserted
+            page_id = stack.back();
+            stack.pop_back();
+            current_head = page_id->load(mo_relax);
+            delta_chain_length = 0;
+            continue;
+
+          case DeltaNodeType::kSplit:
+            // check whether a split right (i.e., sibling) node includes a target key
+            auto meta = current_head->GetMetadata(0);
+            auto sep_key = current_head->GetKey(meta);
+            if (Compare{}(sep_key, key) || (!range_closed && !Compare{}(key, sep_key))) {
+              // traverse to a split right node
+              page_id = current_head->template GetPayload<Mapping_t *>(meta);
+              current_head = page_id->load(mo_relax);
+              delta_chain_length = 0;
+              continue;
+            }
+            break;
+
+          case DeltaNodeType::kMerge:
+            // check whether a merged node includes a target key
+            meta = current_head->GetMetadata(0);
+            sep_key = current_head->GetKey(meta);
+            if (Compare{}(sep_key, key) || (!range_closed && !Compare{}(key, sep_key))) {
+              // traverse to a merged node
+              current_head = current_head->template GetPayload<Node_t *>(meta);
+              ++delta_chain_length;
+              continue;
+            }
+            break;
+
+          case DeltaNodeType::kNotDelta:
+            // reach a base page
+            auto idx = current_head->SearchRecord(key, range_closed);
+            meta = current_head->GetMetadata(idx);
+            page_id = current_head->template GetPayload<Mapping_t *>(meta);
+            goto found_child_node;
+        }
+
+        // go to the next delta record or base node
+        current_head = current_head->GetNextNode();
+        ++delta_chain_length;
+      }
+
+    found_child_node:
+      if (delta_chain_length > kMaxDeltaNodeNum) {
+        consolidate_node = stack.back();
+      }
+      stack.emplace_back(page_id);
+      current_head = page_id->load(mo_relax);
+    }
+  }
+
   /*################################################################################################
    * Internal structure modification functoins
    *##############################################################################################*/
