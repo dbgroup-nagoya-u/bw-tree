dd3a569bf65f709a8ff20c577e743fdc982e01dd
Kento Sugiura <sugiura@db.is.i.nagoya-u.ac.jp>
2021-09-08 14:33:41 +0900

    add fundamental structure for Bw-tree

--- include/bw_tree/bw_tree.hpp
+++ include/bw_tree/bw_tree.hpp
@@ -15,3 +15,82 @@
  */
 
 #pragma once
+
+#include <functional>
+#include <memory>
+#include <utility>
+#include <vector>
+
+#include "component/node.hpp"
+#include "memory/epoch_based_gc.hpp"
+
+namespace dbgroup::index::bw_tree
+{
+/**
+ * @brief A class to represent Bw-tree.
+ *
+ * @tparam Key a target key class.
+ * @tparam Payload a target payload class.
+ * @tparam Compare a comparetor class for keys.
+ */
+template <class Key, class Payload, class Compare = ::std::less<Key>>
+class BwTree
+{
+  using DeltaNodeType = component::DeltaNodeType;
+  using Metadata = component::Metadata;
+  using NodeReturnCode = component::NodeReturnCode;
+  using NodeType = component::NodeType;
+  using Node_t = component::Node<Key, Payload, Compare>;
+  using Mapping_t = std::atomic<Node_t *>;
+  using NodeGC_t = ::dbgroup::memory::EpochBasedGC<Node_t>;
+  using NodeStack_t = std::vector<Node_t *, ::dbgroup::memory::STLAlloc<Node_t *>>;
+  using Binary_p = std::unique_ptr<std::remove_pointer_t<Payload>,
+                                   ::dbgroup::memory::Deleter<std::remove_pointer_t<Payload>>>;
+
+ private:
+  /*################################################################################################
+   * Internal constants
+   *##############################################################################################*/
+
+  static constexpr auto mo_relax = component::mo_relax;
+
+  /*################################################################################################
+   * Internal member variables
+   *##############################################################################################*/
+
+  /// a root node of Bw-tree
+  Mapping_t *root_;
+
+  /// garbage collector
+  NodeGC_t gc_;
+
+  /*################################################################################################
+   * Internal utility functions
+   *##############################################################################################*/
+
+  /*################################################################################################
+   * Internal structure modification functoins
+   *##############################################################################################*/
+
+  void
+ public:
+  /*################################################################################################
+   * Public constructor/destructor
+   *##############################################################################################*/
+
+  BwTree(const BwTree &) = delete;
+  BwTree &operator=(const BwTree &) = delete;
+  BwTree(BwTree &&) = delete;
+  BwTree &operator=(BwTree &&) = delete;
+
+  /*################################################################################################
+   * Public read APIs
+   *##############################################################################################*/
+
+  /*################################################################################################
+   * Public write APIs
+   *##############################################################################################*/
+
+};
+
+}  // namespace dbgroup::index::bw_tree
