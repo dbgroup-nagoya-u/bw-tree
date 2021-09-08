88baa61a706a7315978b5c4ea4fad2f5651d9ec9
Kento Sugiura <sugiura@db.is.i.nagoya-u.ac.jp>
2021-09-08 14:35:16 +0900

    add dummy Bw-tree test

---
+++ test/bw_tree_test.cpp
@@ -0,0 +1,148 @@
+/*
+ * Copyright 2021 Database Group, Nagoya University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "bw_tree/bw_tree.hpp"
+
+#include <memory>
+#include <vector>
+
+#include "common.hpp"
+#include "gtest/gtest.h"
+
+namespace dbgroup::index::bw_tree::test
+{
+// use a supper template to define key-payload pair templates
+template <class KeyType, class PayloadType, class KeyComparator, class PayloadComparator>
+struct KeyPayload {
+  using Key = KeyType;
+  using Payload = PayloadType;
+  using KeyComp = KeyComparator;
+  using PayloadComp = PayloadComparator;
+};
+
+template <class KeyPayload>
+class NodeFixture : public testing::Test
+{
+ protected:
+  // extract key-payload types
+  using Key = typename KeyPayload::Key;
+  using Payload = typename KeyPayload::Payload;
+  using KeyComp = typename KeyPayload::KeyComp;
+  using PayloadComp = typename KeyPayload::PayloadComp;
+
+  // define type aliases for simplicity
+  using Node_t = component::Node<Key, Payload, std::less<Key>>;
+  using Metadata = component::Metadata;
+
+  /*################################################################################################
+   * Internal constants
+   *##############################################################################################*/
+
+  static constexpr size_t kKeyNumForTest = 1024;
+  static constexpr size_t kKeyLength = kWordLength;
+  static constexpr size_t kPayloadLength = kWordLength;
+
+  /*################################################################################################
+   * Internal member variables
+   *##############################################################################################*/
+
+  // actual keys and payloads
+  size_t key_length;
+  size_t payload_length;
+  Key keys[kKeyNumForTest];
+  Payload payloads[kKeyNumForTest];
+
+  // the length of a record and its maximum number
+  size_t record_length;
+  size_t max_record_num;
+
+  // a target node and its expected metadata
+  std::unique_ptr<Node_t> node;
+
+  /*################################################################################################
+   * Setup/Teardown
+   *##############################################################################################*/
+
+  void
+  SetUp() override
+  {
+    // prepare keys
+    key_length = (IsVariableLengthData<Key>()) ? 7 : sizeof(Key);
+    PrepareTestData(keys, kKeyNumForTest, key_length);
+
+    // prepare payloads
+    payload_length = (IsVariableLengthData<Payload>()) ? 7 : sizeof(Payload);
+    PrepareTestData(payloads, kKeyNumForTest, payload_length);
+
+    // set a record length and its maximum number
+    record_length = key_length + payload_length;
+    max_record_num = (kPageSize - component::kHeaderLength) / (record_length + sizeof(Metadata));
+    if (max_record_num > kKeyNumForTest) max_record_num = kKeyNumForTest;
+  }
+
+  void
+  TearDown() override
+  {
+    ReleaseTestData(keys, kKeyNumForTest);
+    ReleaseTestData(payloads, kKeyNumForTest);
+  }
+
+  /*################################################################################################
+   * Functions for verification
+   *##############################################################################################*/
+
+  void
+  VerifyKey(  //
+      const size_t idx,
+      const Metadata meta)
+  {
+    auto key = node->GetKey(meta);
+    EXPECT_TRUE(component::IsEqual<KeyComp>(key, keys[idx]));
+  }
+
+  void
+  VerifyPayload(  //
+      const size_t idx,
+      const Metadata meta)
+  {
+    Payload payload{};
+    node->CopyPayload(meta, payload);
+    EXPECT_TRUE(component::IsEqual<PayloadComp>(payload, payloads[idx]));
+
+    if constexpr (IsVariableLengthData<Payload>()) {
+      ::dbgroup::memory::Delete(payload);
+    }
+  }
+};
+
+/*##################################################################################################
+ * Preparation for typed testing
+ *################################################################################################*/
+
+using KeyPayloadPairs = ::testing::Types<KeyPayload<uint64_t, uint64_t, UInt64Comp, UInt64Comp>,
+                                         KeyPayload<char *, uint64_t, CStrComp, UInt64Comp>,
+                                         KeyPayload<uint64_t, char *, UInt64Comp, CStrComp>,
+                                         KeyPayload<char *, char *, CStrComp, CStrComp>,
+                                         KeyPayload<uint32_t, uint64_t, UInt32Comp, UInt64Comp>,
+                                         KeyPayload<uint32_t, uint32_t, UInt32Comp, UInt32Comp>,
+                                         KeyPayload<uint64_t, uint64_t *, UInt64Comp, PtrComp>>;
+TYPED_TEST_CASE(NodeFixture, KeyPayloadPairs);
+
+/*##################################################################################################
+ * Unit test definitions
+ *################################################################################################*/
+
+}  // namespace dbgroup::index::bw_tree::test
